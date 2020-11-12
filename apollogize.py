d396411168d8ae657a339c750ab6df3583c7b909
Zac Chien <fengying0709@gmail.com>
2020-11-12 14:44:06 +0800

    first refactor

--- apollogize.py
+++ apollogize.py
@@ -2,141 +2,147 @@
     Usage:
         python3.7 apollogize.py <user_name> <password> (<on_broad_data>)
 '''
-import sys
+import argparse
+import logging
 import time
 from datetime import datetime
 from random import randint
 
+import pendulum
 import requests
-from dateutil.relativedelta import relativedelta
 
-company_id = 'bb04f185-9731-4348-a0e0-6834fe5dff58'
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+)
 
-def gen_cookies(username, password) -> requests.cookies.RequestsCookieJar:
-    resp_token = requests.post(url='https://auth.mayohr.com/Token', data={
-        'username': username,
-        'password': password,
-        'grant_type': 'password',
-    })
-
-    print(resp_token)
-
-    payload_auth = {
-        'code': resp_token.json()['code'],
-        'CompanyId': company_id,
-    }
-    resp_pass = requests.get(url='https://authcommon.mayohr.com/api/auth/checkticket', params=payload_auth)
-    return resp_pass.cookies
+LOGGER = logging.getLogger(__file__)
 
 
-def auto_failure_check(
-    cookies: requests.cookies.RequestsCookieJar,
-    work_date: str,
-    start_hour: int = 2,
-    end_hour: int = 11,
-) -> str:
-    url = 'https://pt.mayohr.com/api/reCheckInApproval'
-    payload_in = {
-        'AttendanceOn': f'{work_date}T{start_hour:02d}:{randint(0, 30):02d}:00+00:00',
-        'AttendanceType': 1,
-        'PunchesLocationId': 'e506b866-49f3-4c47-a324-cd8c4fa7b580',
-        'IsBehalf': False,
-    }
-    resp_in = requests.post(url=url, cookies=cookies, data=payload_in)
-    err_in = resp_in.json().get('Error', {}).get('Title', None)
-    print(
-        f'check in {payload_in['AttendanceOn']} '
-        f'(code={resp_in.status_code}, err={err_in})'
-    )
-    time.sleep(2)
+class Apollogize:
 
-    payload_out = {
-        'AttendanceOn': f'{work_date}T{end_hour:02d}:{randint(0, 59):02d}:00+00:00',
-        'AttendanceType': 2,
-        'PunchesLocationId': 'e506b866-49f3-4c47-a324-cd8c4fa7b580',
-        'IsBehalf': False,
-    }
-    resp_out = requests.post(url=url, cookies=cookies, data=payload_out)
-    err_out = resp_out.json().get('Error', {}).get('Title', None)
-    print(
-        f'check out {payload_out['AttendanceOn']} '
-        f'(code={resp_out.status_code}, err={err_out})'
-    )
-    time.sleep(2)
+    COMPANY_ID = 'bb04f185-9731-4348-a0e0-6834fe5dff58'
+    PUNCHES_LOCATION_ID = 'e506b866-49f3-4c47-a324-cd8c4fa7b580'
 
-    if err_out == 'Check out time cannot be earlier than check in time':
-        return work_date
+    def __init__(self, username, password, starting_dt):
+        self.cookies = self.gen_cookies(username, password)
+        self.starting_dt = starting_dt
+        self.fail_dts = list()
 
+    @property
+    def success(self):
+        return not self.fail_dts
 
-def main(username: str, password: str, cal: datetime.date):
-    cookies = gen_cookies(username=username, password=password)
-    url_cal = 'https://pt.mayohr.com/api/EmployeeCalendars/scheduling'
-    payload_cal = {'year': cal.year, 'month': cal.month}
-    resp = requests.get(url=url_cal, params=payload_cal, cookies=cookies)
-    failed = []
-    for day in resp.json()['Data']['Calendars']:
-        # from pprint import pprint
-        # pprint(day)
-        # continue
+    @property
+    def fail_dts(self):
+        return self.fail_dts
 
-        work_day = datetime.strptime(day['Date'][:10], '%Y-%m-%d')
-        if work_day <= datetime.now() and day['ShiftSchedule']:
-            is_on_duty = False if day['LeaveSheets'] else True
-            is_work_day = day['ShiftSchedule']['WorkOnTime'] is not None
-            is_on_board = work_day >= on_board
-            result = None
-            if is_work_day and is_on_duty and is_on_board:
-                result = auto_failure_check(
-                    cookies=cookies, work_date=str(work_day.date())
-                )
-            elif is_work_day and is_on_board and not is_on_duty:
-                leave_start = datetime.fromisoformat(
-                    day['LeaveSheets'][0]['LeaveStartDatetime']
-                )
-                leave_end = datetime.fromisoformat(
-                    day['LeaveSheets'][0]['LeaveEndDatetime']
-                )
-                work_hours = set(range(2, 12))
-                adjust_work_hour = work_hours.difference(
-                    set(range(leave_start.hour, leave_end.hour + 1))
-                )
-                if len(adjust_work_hour) > 0:
-                    result = auto_failure_check(
-                        cookies=cookies,
-                        work_date=str(work_day.date()),
-                        start_hour=min(adjust_work_hour),
-                        end_hour=max(adjust_work_hour),
+    def gen_cookies(
+        self, username, password
+    ) -> requests.cookies.RequestsCookieJar:
+        resp_token = requests.post(
+            url='https://auth.mayohr.com/Token',
+            data={
+                'username': username,
+                'password': password,
+                'grant_type': 'password',
+            },
+        )
+
+        if resp_token.status_code != 200:
+            LOGGER.error('Wrong username or password!')
+            exit(1)
+
+        resp_pass = requests.get(
+            url='https://authcommon.mayohr.com/api/auth/checkticket',
+            params={
+                'code': resp_token.json()['code'],
+                'CompanyId': self.COMPANY_ID,
+            },
+        )
+        return resp_pass.cookies
+
+    def do_recheckin(self, att_type: int, work_dt: str, hour: str):
+        att_time = f'{work_dt}T{hour:02d}:{randint(0, 30):02d}:00+00:00'
+        resp = requests.post(
+            url='https://pt.mayohr.com/api/reCheckInApproval',
+            cookies=self.cookies,
+            data={
+                'AttendanceOn': att_time,
+                'AttendanceType': att_type,
+                'PunchesLocationId': self.PUNCHES_LOCATION_ID,
+                'IsBehalf': False,
+            },
+        )
+
+        time.sleep(2)
+        err = resp.json().get('Error', {}).get('Title')
+        if resp.status_code == 200:
+            LOGGER.INFO(f'{att_time} att_type={att_type} success!')
+        else:
+            LOGGER.ERROR(f'{att_time} (code={resp.status_code}, err={err})')
+            raise Exception(err)
+
+    def get_work_dates(self, dt: pendulum.datetime.DateTime):
+        resp = requests.get(
+            url='https://pt.mayohr.com/api/EmployeeCalendars/scheduling',
+            params={'year': dt.year, 'month': dt.month},
+            cookies=self.cookies,
+        )
+        for dt_cal in resp.json()['Data']['Calendars']:
+            work_dt = datetime.strptime(dt_cal['Date'][:10], '%Y-%m-%d')
+            is_valid_dt = work_dt <= datetime.now() and dt['ShiftSchedule']
+            exist_leave = False if dt_cal['LeaveSheets'] else True
+            is_work_day = dt_cal['ShiftSchedule']['WorkOnTime'] is not None
+            is_in_range = dt_cal >= self.starting_dt
+
+            if is_valid_dt and is_work_day and is_in_range:
+                s_hour = 2  # 10 AM in Taipei time
+                e_hour = 11  # 7 PM in Taipei time
+                if exist_leave:
+                    leave_start = pendulum.parse(dt_cal['LeaveSheets'][0]['LeaveStartDatetime'])
+                    leave_end = pendulum.parse(dt_cal['LeaveSheets'][0]['LeaveEndDatetime'])
+                    adjust_work_hour = set(range(2, 12)).difference(
+                        set(range(leave_start.hour, leave_end.hour + 1))
                     )
-                print(
-                    f'{work_day.date()} requested a leave, '
-                    f'leave_start={leave_start.time()}, leave_end={leave_end.time()}'
-                )
+                    if adjust_work_hour:
+                        LOGGER.INFO(
+                            f'leave on {work_dt}, start={leave_start.time()}, end={leave_end.time()}'
+                        )
+                        yield dt_cal, min(adjust_work_hour), max(adjust_work_hour)
 
-            if result:
-                failed.append(result)
-    return failed
+                yield dt_cal, s_hour, e_hour
+
+    def process(self, dt: pendulum.datetime.DateTime):
+        for dt, s_hour, e_hour in self.get_work_dates(dt):
+            try:
+                self.do_recheckin(1, dt, s_hour)  # recheckin work on
+                self.do_recheckin(2, dt, e_hour)  # recheckin work off
+            except:
+                self.fail_dts.append(dt)
 
 
 if __name__ == '__main__':
-    on_board = datetime.strptime(
-        f'{datetime.now().year}-01-02' if len(sys.argv) <= 3 else sys.argv[3],
-        '%Y-%m-%d',
-    )
-    current_cal = datetime(datetime.now().year, 1, 1)
-    err_date = []
-    while current_cal <= datetime.now():
-        err_date.extend(
-            main(
-                username=f'{sys.argv[1]}@gogolook.com',
-                password=sys.argv[2],
-                cal=current_cal.date(),
-            )
-        )
-        current_cal += relativedelta(months=1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', '-u', required=True)
+    parser.add_argument('--password', '-p', required=True)
+    parser.add_argument('--starting_dt', '-s')
+    args = parser.parse_args()
 
-    if err_date:
-        print(
-            f'{sys.argv[1]} please double check following data: {', '.join(err_date)}'
-        )
-    else:
-        print('Done!')
+    start = pendulum.now().first_of('year')
+    end = pendulum.now()
+    aplo = Apollogize(
+        username=f'{args.username}@gogolook.com',
+        password=args.password,
+        starting_dt=pendulum.parse(args.starting_dt) if args.starting_dt else start.to_date_string(),
+    )
+
+    for dt_month in pendulum.period(start, end).range('months'):
+        aplo.process(dt_month)
+
+    # if aplo.success:
+    #     LOGGER.INFO('Success!')
+    # else:
+    #     LOGGER.ERROR(
+    #         f"Please double check following data: {', '.join(aplo.fail_dts)}"
+    #     )
